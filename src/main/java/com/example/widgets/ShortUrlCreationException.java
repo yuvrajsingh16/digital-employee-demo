43d0e32a0f41993d4b144a2c127030e7bea1b3d1
yuvrajsingh16 <19yuvrajsingh90@gmail.com>
2026-07-10 14:04:16 +0530

    DE-109: add bounded short URL creation failure

---
+++ src/main/java/com/example/widgets/ShortUrlCreationException.java
@@ -0,0 +1,8 @@
+package com.example.widgets;
+
+public class ShortUrlCreationException extends RuntimeException {
+
+    public ShortUrlCreationException(String message) {
+        super(message);
+    }
+}
